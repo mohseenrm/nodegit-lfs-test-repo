c111b67270fcf6e3ac066a5a58a4eb8aa7b2262f
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-07-13 11:25:06 -0700

    updating repo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const NodeGit = require('nodegit');
-
 const NodeGitLfs = require('nodegit-lfs')(NodeGit);
+const exec = require('./execHelper');
 
 console.log('NodeGitLFS: ', NodeGitLfs);
 
@@ -11,4 +11,6 @@ const NodeGitLFS = NodeGitLfs.then((ng) => {
 	return ng;
 })
 	.then((ng) => ng.LFS.initialize(process.cwd()))
-	.then(() => fs.appendFileSync(path.join(process.cwd(), '.gitattributes'), '*.txt filter=lfs\n'));
+	// .then(() => fs.appendFileSync(path.join(process.cwd(), '.gitattributes'), '*.txt filter=lfs\n'));
+	.then(() => exec('base64 /dev/urandom | head -c 200000000 > big_file_test.txt'))
+	.then((process, stdin, stdout) => console.log(`[DEBUG]{Process}: ${process}\n\n{stdin}: ${stdin}\n\n{stdout}: ${stdout}`));
