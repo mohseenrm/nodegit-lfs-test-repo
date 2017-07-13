0aaebf13ab5432ec99ea365abc19c3ff5aeecd8f
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-07-13 10:07:04 -0700

    adding large file to test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const NodeGit = require('nodegit');
 
 const NodeGitLfs = require('nodegit-lfs')(NodeGit);
@@ -7,4 +9,6 @@ console.log('NodeGitLFS: ', NodeGitLfs);
 const NodeGitLFS = NodeGitLfs.then((ng) => {
 	console.log('NodeGitLFS: ', ng.LFS);
 	return ng;
-});
+})
+	.then((ng) => ng.LFS.initialize(process.cwd()))
+	.then(() => fs.appendFileSync(path.join(process.cwd(), '.gitattributes'), '*.txt filter=lfs\n'));
